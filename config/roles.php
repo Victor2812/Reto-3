ee49af4dad14484674deac03d3c9275e47261a7d
6e0d0a <gaizka.gg@outlook.es>
2023-01-25 09:52:29 +0100

    rellenar roles en la base de datos

---
+++ config/roles.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Roles de la aplicación
+|--------------------------------------------------------------------------
+|
+| Estos roles se utilizan para identificar cada tipo de persona registrada
+| en la aplicación.
+|
+*/
+
+return [
+//  ID      Nombre
+    1 =>    'COORDINADOR',
+    2 =>    'FACTILITADOR_ACADEMICO',
+    3 =>    'FACILITADOR_EMPRESA',
+    4 =>    'ALUMNO',
+];
