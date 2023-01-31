24be61bc5b50447b6092418513458d2fb2c6b9aa
6e0d0a <gaizka.gg@outlook.es>
2023-01-31 16:36:43 +0100

    filtro parcial de los tutores

--- app/Http/Controllers/TutorsController.php
+++ app/Http/Controllers/TutorsController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use App\Models\Role;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TutorsController extends Controller
 {
@@ -14,10 +17,58 @@ class TutorsController extends Controller
      */
     public function index(Request $request)
     {
+        // Información de la base de datos
+        $tutors = Person::allTutors();
+        $roles = Role::all();
+        // TODO: grados
+        // TODO: compañías
+
+        // Obtener filtros
+        $search = $request->query('search');
+        $role = (int)$request->query('role');
+        $grade = (int)$request->query('grade');
+        $company = (int)$request->query('company');
+
+        // Aplicar texto de búsqueda
+        if ($search) {
+            $search_terms = explode(' ', trim($search));
+            $search_terms = array_filter($search_terms, function ($param) {
+                return strlen($param) > 0;
+            });
+
+            $tutors->where(function (Builder $query) use ($search_terms) {
+                foreach ($search_terms as $term) {
+                    $query->orWhere(DB::raw("LOWER(name)"), 'like', "%$term%")
+                        ->orWhere(DB::raw("LOWER(surname)"), 'like', "%$term%")
+                        ->orWhere(DB::raw("LOWER(dni)"), 'like', "%$term%");
+                }
+            });
+        }
+
+        if ($role > 0) {
+            $tutors->where('role_id', '=', $role);
+        }
+
+        if ($grade > 0) {
+            // ...
+        }
+
+        if ($company > 0) {
+            // ...
+        }
+
         // Devuelve la lista de tutores
-        $tutors = Person::allTutors()->get();
         return view('tutors.index', [
-            'tutors' => $tutors,
+            // Información de la base de datos
+            'tutors' => $tutors->get(),
+            'roles' => $roles,
+            'grades' => [],
+            'companies' => [],
+            // Términos de búsqueda previos
+            'old_search' => $search, // mostrar selección actual si la hay
+            'old_role' => $role,
+            'old_grade' => $grade,
+            'old_company' => $company,
         ]);
     }
 
--- resources/views/tutors/index.blade.php
+++ resources/views/tutors/index.blade.php
@@ -10,6 +10,37 @@
         </a>
     </div>
 
+    <div>
+        <form action="{{ route(Route::currentRouteName()) }}" method="GET">
+            <input type="text" name="search" placeholder="Nombre, Apellido, DNI ..." value="{{ $old_search }}"/>
+            <select name="role">
+                <option value="">-- Cualquiera --</option>
+                @foreach ($roles as $r)
+                    <option value="{{ $r->id }}" @selected($r->id == $old_role)>
+                        {{ $r->name }}
+                    </option>
+                @endforeach
+            </select>
+            <select name="grade">
+                <option value="">-- Cualquiera --</option>
+                @foreach ($grades as $g)
+                    <option value="{{ $g->id }}"  @selected($g->id == $old_grade)>
+                        {{ $g->name }}
+                    </option>
+                @endforeach
+            </select>
+            <select name="company">
+                <option value="">-- Cualquiera --</option>
+                @foreach ($companies as $c)
+                <option value="{{ $c->id }}"  @selected($c->id == $old_company)>
+                    {{ $c->name }}
+                </option>
+                @endforeach
+            </select>
+            <button type="submit"><i class="bi bi-search"></i></button>
+        </form>
+    </div>
+
     <div class="row">
         <div class="col-12 px-4">
             <div class="table-responsive">
